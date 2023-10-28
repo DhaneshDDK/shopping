d9ac4ee0194d7f3efb700c6248a02387c6de42cc
DhaneshDDK <dandedhanesh899@gmail.com>
2023-10-28 16:43:33 +0530

    commit-4

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,6 +1,6 @@
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 
 export const apiEndPoints = {
     LOGIN_API : BASE_URL + "/auth/login",
