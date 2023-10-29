267452f76bc24b51438a5498942c6f572f1f933c
DhaneshDDK <dandedhanesh899@gmail.com>
2023-10-29 13:59:04 +0530

    commit-8

--- src/Pages.jsx/SingUpPage.jsx
+++ src/Pages.jsx/SingUpPage.jsx
@@ -38,7 +38,7 @@ const SingUpPage = () => {
     <div className='text-white text-[28px] mx-auto font-bold'>Welcome to Ecomzy</div>
 
       <form onSubmit={submitHandler}
-       className='w-[670px] mx-auto  h-[400px] flex flex-col gap-y-6 rounded-md py-6 px-4 ' >
+       className='w-[100%] md:w-[670px]  mx-auto h-[400px] flex flex-col gap-y-6 rounded-md py-6 px-0 ' >
        
        <input type="text"
        placeholder='Enter your name'
